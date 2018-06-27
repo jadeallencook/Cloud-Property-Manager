60faa9ff294cd0a385de0fd1c4e23b5434704fca
Jade Allen Cook <jadeallencook@gmail.com>
2018-06-26 19:59:08 -0600

    🔥 Fired up initial CPM Angular boilerplate

---
+++ dev/src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { SettingsComponent } from './pages/settings/settings.component';
+import { BlogsComponent } from './pages/blogs/blogs.component';
+import { BannerComponent } from './pages/banner/banner.component';
+import { FeaturedComponent } from './pages/featured/featured.component';
+import { NavbarComponent } from './componenets/navbar/navbar.component';
+
+const appRoutes: Routes = [
+  { path: 'settings', component: SettingsComponent },
+  { path: 'blogs', component: BlogsComponent },
+  { path: 'banner', component: BannerComponent },
+  { path: 'featured', component: FeaturedComponent },
+  { path: '**', component: SettingsComponent }
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SettingsComponent,
+    BlogsComponent,
+    BannerComponent,
+    FeaturedComponent,
+    NavbarComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
